c4055d88df01d654c84f56174c206d561bbda1b0
amirmohammad <kavkaniamirmohammad@gmail.com>
2021-10-13 08:41:42 +0330

    very_easy_table_output

---
+++ very_easy_table.py
@@ -0,0 +1,15 @@
+very_easy_table_answare = [
+    2, 3, 4, 1,
+    4, 1, 2, 3,
+    3, 2, 1, 4,
+    1, 4, 3, 2
+]
+
+very_easy_table = [
+    [None, 3, 4, 1],
+    [4, 1, 2, None],
+    [3, 2, 1, None],
+    [None, 4, 3, 2]
+]
+
+print(very_easy_table)
