c2b5d41af5f809182151f7e07226411645b24fa5
amirmohammad <kavkaniamirmohammad@gmail.com>
2021-10-28 10:26:39 +0330

    input the audio
    input the audio for process on audio

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import very_easy_table
 import sodoko
+import pyaudio
+import wave
+import sys
 
-sodoko.sodokoInput(very_easy_table.very_easy_table, very_easy_table.bigest)
+sodoko.sodokoInput(very_easy_table.very_easy_table, very_easy_table.bigest, 4 * 4)
