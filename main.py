4ac1be00de3bcfd9c6a02743f4e3de9cd0b84aa1
amirmohammad <kavkaniamirmohammad@gmail.com>
2021-12-08 08:58:26 +0330

    Sudoku generator
    The simple AI That generates a random sudoku in different difficulties.
    #Random library
    #numpy library

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import very_easy_table
-import check_value
+import seasy
 import audio_input
 import audio_procces
 
-check_value.sodokoInput(very_easy_table.table1, very_easy_table.bigest, 4 * 4)
-check_value.sodokoInput(very_easy_table.table2, very_easy_table.bigest, 4 * 4)
-check_value.sodokoInput(very_easy_table.table3, very_easy_table.bigest, 4 * 4)
-check_value.sodokoInput(very_easy_table.table4, very_easy_table.bigest, 4 * 4)
-check_value.sodokoInput(very_easy_table.table5, very_easy_table.bigest, 4 * 4)
-check_value.sodokoInput(very_easy_table.table6, very_easy_table.bigest, 4 * 4)
+seasy.sodokoInput(very_easy_table.table1, very_easy_table.bigest, 2)
+seasy.sodokoInput(very_easy_table.table2, very_easy_table.bigest, 2)
+seasy.sodokoInput(very_easy_table.table3, very_easy_table.bigest, 2)
+seasy.sodokoInput(very_easy_table.table4, very_easy_table.bigest, 2)
+seasy.sodokoInput(very_easy_table.table5, very_easy_table.bigest, 2)
+seasy.sodokoInput(very_easy_table.table6, very_easy_table.bigest, 2)
--- very_easy_table.py
+++ very_easy_table.py
@@ -1,42 +1,42 @@
-bigest = 4
+bigest = 10
 table1 = [
-    7, None, None, None,
-    None, 6, None, None,
-    None, None, None, None,
-    2, None, None, None
+    [7, None, None, None],
+    [None, 6, None, None],
+    [None, None, None, None],
+    [2, None, None, None]
 ]
 
 table2 = [
-    None, None, None, 6,
-    1, 4, 7, None,
-    2, None, None, None,
-    None, None, 8, 1
+    [None, None, None, 6],
+    [1, 4, 7, None],
+    [2, None, None, None],
+    [None, None, 8, 1]
 ]
 
 table3 = [
-    None, None, None, 7,
-    None, None, None, 6,
-    None, None, 5, None,
-    8, None, 3, None
+    [None, None, None, 7],
+    [None, None, None, 6],
+    [None, None, 5, None],
+    [8, None, 3, None]
 ]
 
 table4 = [
-    None, 5, None, None,
-    None, None, None, None,
-    None, 7, None, None,
-    None, None, 4, None
+    [None, 5, None, None],
+    [None, None, None, None],
+    [None, 7, None, None],
+    [None, None, 4, None]
 ]
 
 table5 = [
-    None, None, None, None,
-    None, 1, 4, 6,
-    None, 8, None, 5,
-    6, None, 4, 1
+    [None, None, None, None],
+    [None, 1, 4, 6],
+    [None, 8, None, 5],
+    [6, None, 4, 1]
 ]
 
 table6 = [
-    None, 2, None, None,
-    None, None, None, 7,
-    3, None, None, None,
-    None, None, None, None
+    [None, 2, None, None],
+    [None, None, None, 7],
+    [3, None, None, None],
+    [None, None, None, None]
 ]
