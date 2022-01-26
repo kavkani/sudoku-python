d7aae3e3e701ebacc762d2cd2df8c83e7f9ad215
ArashOstadsharif <88422080+ArashOstadsharif@users.noreply.github.com>
2022-01-26 09:23:57 +0330

    easy insert
    I added a def that return the next ID. so there `s no need to have an Id parameter so it makes it easier to work with. also I found a better way to return data.

--- db.py
+++ db.py
@@ -1,11 +1,35 @@
 import pyodbc
+def count_ID(table):
+    #connecting to server
+    con = pyodbc.connect('DRIVER={SQL Server}; SERVER=DESKTOP-UT46DSN; Database=Sudoku; UID=sa; PWD=1234;')
+    #making command executer
+    cursor = con.cursor()
+    #making the command
+    command = "SELECT count(ID) FROM "+table
+    #runing the command
+    cursor.execute(command)
+    #making data readabla
+    data = []
+    row = cursor.fetchone() 
+    while row: 
+        for i in row:
+            data.append(i)
+        row = cursor.fetchone()
+    #closing conection
+    con.close()
+    #returning
+    return data[0]+1
+    
+    
+
 def insert(values,table):
     #connecting to server
     con = pyodbc.connect('DRIVER={SQL Server}; SERVER=DESKTOP-UT46DSN; Database=Sudoku; UID=sa; PWD=1234;')
     #making command executer
     cursor = con.cursor()
-    #making the command 
-    command = "insert into "+table+" VALUES("
+    #making the command
+    ID = str(count_ID('Users'))
+    command = "insert into "+table+" VALUES("+ID+','
     for i in range(len(values)-1):
         if type(values[i]) == str:
             command += "'"
@@ -34,11 +58,19 @@ def select(ID,table,info):
     command = "SELECT "+info+" FROM "+table+" WHERE ID = "+ID
     #runing the command
     cursor.execute(command)
-    #making the data readable and returning it
-    data = cursor.fetchall()
-    return data
+    #making the data readable
+    data = []
+    row = cursor.fetchone() 
+    while row:
+        line = []
+        for i in row:
+            line.append(i)
+        data.append(line)
+        row = cursor.fetchone()
     #closing conection
     con.close()
+    #returning
+    return data
 def delete(table , ID):
     #connecting to server
     con = pyodbc.connect('DRIVER={SQL Server}; SERVER=DESKTOP-UT46DSN; Database=Sudoku; UID=sa; PWD=1234;')
@@ -52,4 +84,3 @@ def delete(table , ID):
     con.commit()
     #closing conection
     con.close()
-delete('Users','2')
