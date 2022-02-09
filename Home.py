923a2bc79ef579d18e40947fae673147fb625e35
Bardia-Sohrabi <bardiasohrabi86@gmail.com>
2022-02-09 09:18:36 +0330

    Colors updated

--- Home.py
+++ Home.py
@@ -9,20 +9,21 @@ app = Ursina()
 
 def main(code=0):
     window.fullscreen = True
+    Sky(texture="bg")
     if code == 1:
         scene.clear()
-    tutorial = Button(icon='video', scale=0.13, position=(-0.8, 0.38), color=color.white)
+    tutorial = Button(icon='video', scale=0.13, position=(-0.7, 0.35), color=rgb(83, 221, 108))
     tutorial.tooltip = Tooltip("Tutorial")
-    account = Button(icon="account", scale=0.13, position=(0.8, 0.38), color=color.white)
+    account = Button(icon="account", scale=0.13, position=(0.7, 0.35), color=rgb(83, 221, 108))
     account.tooltip = Tooltip("Account")
-    new_game = Button(icon="sudoku2", text="Start a New 3D Sudoku", text_origin=(0, -0.48), scale=(0.5, 0.6),
-                      color=color.blue)
+    new_game = Button(icon="s4", text="Start a New 3D Sudoku", text_origin=(0, -0.45), scale=(0.54, 0.63),
+                      color=rgb(64, 71, 109))
     new_game.on_click = Func(game)
 
 
 def game():
     scene.clear()
-    Sky(texture="bg5")
+    Sky(texture="bg")
 
     def checker():
         result = []
@@ -45,7 +46,7 @@ def game():
         else:
             scene.clear()
             t = Entity(model='quad', text="You did it!")
-            menu = Button(text="Back to Main Menu", position=(0.6, -0.36), color=color.orange, scale=(0.4, 0.08))
+            menu = Button(text="Back to Main Menu", position=(0.6, -0.36), color=rgb(64, 71, 109), scale=(0.4, 0.08))
             menu.on_click = Func(main, 1)
 
     def answer():
@@ -53,7 +54,7 @@ def game():
         for a in range(9):
             r2 = []
             for b in range(9):
-                if b_list[a * 9 + b].text == None or b_list[a * 9 + b].text_color == color.white:
+                if b_list[a * 9 + b].text == None or b_list[a * 9 + b].text_color == rgb(193, 202, 214):
                     r2.append(0)
                 else:
                     r2.append(int(b_list[a * 9 + b].text))
@@ -68,18 +69,18 @@ def game():
         for a in range(9):
             for b in range(9):
                 b_list[a * 9 + b].text = str(solved[a][b])
-                b_list[a * 9 + b].text_color = color.lime
+                b_list[a * 9 + b].text_color = rgb(191, 255, 0)
                 b_list[a * 9 + b].disabled = True
-        menu = Button(text="Back to Main Menu", position=(0.6, -0.36), color=color.orange, scale=(0.4, 0.08))
+        menu = Button(text="Back to Main Menu", position=(0.6, -0.36), color=rgb(64, 71, 109), scale=(0.4, 0.08))
         menu.on_click = Func(main, 1)
 
     def enable(bx=-1, t=0):
         if bx != -1:
-            b_list[bx].color = color.black
+            b_list[bx].color = rgb(251, 77, 61)
             if t != 0:
                 b_list[bx].text = t
         for item in b_list:
-            if item.text == None or item.text_color == color.white:
+            if item.text == None or item.text_color == rgb(193, 202, 214):
                 item.disabled = False
         for num in nums:
             num.disabled = True
@@ -88,11 +89,11 @@ def game():
         check_b.disabled = False
 
     def click(b, bx):
-        if b.text == None or b.text_color == color.white:
+        if b.text == None or b.text_color == rgb(193, 202, 214):
             for item in b_list:
                 if item != b_list[bx]:
                     item.disabled = True
-            b_list[bx].color = color.yellow
+            b_list[bx].color = rgb(83, 221, 108)
             for num in nums:
                 num.disabled = False
                 num.on_click = Func(enable, bx, num.text)
@@ -103,18 +104,20 @@ def game():
         return 0
 
     b_list = []
-    mic = Button(icon="mic", scale=0.06, position=(0.6, -0.215), color=color.blue, disabled=True)
-    deselect = Button(icon="deselect", scale=0.06, position=(0.6, -0.29), color=color.blue, disabled=True)
-    check_b = Button(text="Check", position=(0.6, -0.37), scale=(0.16, 0.08), color=color.orange)
+    mic = Button(icon="mic", scale=0.06, position=(0.6, -0.215), color=rgb(83, 221, 108), disabled=True)
+    mic.pressed_color = rgb(83, 221, 108)
+    deselect = Button(icon="deselect", scale=0.06, position=(0.6, -0.29), color=rgb(83, 221, 108), disabled=True)
+    deselect.pressed_color = rgb(83, 221, 108)
+    check_b = Button(text="Check", position=(0.6, -0.37), scale=(0.16, 0.08), color=rgb(64, 71, 109))
     check_b.on_click = Func(checker)
-    solve = Button(text="Show Answer", position=(0.6, -0.46))
+    solve = Button(text="Show Answer", position=(0.6, -0.46), color=rgb(64, 71, 109))
     solve.fit_to_text(radius=0.005)
     solve.on_click = Func(answer)
     y = 0.46
     nums = []
     for i in range(9):
-        number = Button(text=str(i + 1), scale=0.06, position=(0.6, y), color=color.blue, disabled=True)
-        number.pressed_color = color.blue
+        number = Button(text=str(i + 1), scale=0.06, position=(0.6, y), color=rgb(83, 221, 108), disabled=True)
+        number.pressed_color = rgb(83, 221, 108)
         nums.append(number)
         y -= 0.075
     y = 0.441
@@ -122,11 +125,11 @@ def game():
     for i in range(9):
         x = -0.575
         for j in range(9):
-            button = Button(scale=0.105, position=(x, y), highlight_color=color.lime, color=color.black,
-                            text_color=color.white)
+            button = Button(scale=0.105, position=(x, y), highlight_color=rgb(191, 255, 0), color=rgb(251, 77, 61),
+                            text_color=rgb(193, 202, 214))
             if final[i][j] != 0:
                 button.text = str(final[i][j])
-                button.text_color = color.lime
+                button.text_color = rgb(191, 255, 0)
                 button.disabled = True
             b_list.append(button)
             button.on_click = Func(click, button, i * 9 + j)
