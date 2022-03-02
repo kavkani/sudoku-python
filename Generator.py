b4ac78f065516bb9956c8cdbd0a76e2c6789f23d
Bardia-Sohrabi <bardiasohrabi86@gmail.com>
2022-03-02 15:46:06 +0330

    3D combined with Generator

--- Generator.py
+++ Generator.py
@@ -2,32 +2,30 @@ import random
 
 
 def solver():
-    def generate_face(x):
-        numbers_list = list(range(1, x + 1))
+    def generate_face(b):
+        numbers_list = list(range(1, b + 1))
         list1 = []
-        for i in range(1, x + 1):
+        for i in range(1, b + 1):
             random_num = random.choice(numbers_list)
             numbers_list.remove(random_num)
             list1.append(random_num)
 
         return list1
 
-    pmd = []
+    pmd2 = []
     x = 9
     for j in range(6):
-        pmd.append(generate_face(x))
-    return pmd
+        pmd2.append(generate_face(x))
+    return pmd2
 
-#1
 
 pmd = solver()
-p = pmd[0]
+"""p = pmd[0]
 m = pmd[1]
 d = pmd[2]
 p2 = pmd[3]
 m2 = pmd[4]
 d2 = pmd[5]
-
 p[random.randrange(0, 9)] = 0
 p[random.randrange(0, 9)] = 0
 m[random.randrange(0, 9)] = 0
@@ -40,12 +38,10 @@ m2[random.randrange(0, 9)] = 0
 m2[random.randrange(0, 9)] = 0
 d2[random.randrange(0, 9)] = 0
 d2[random.randrange(0, 9)] = 0
-
 pm = []
 pm.append(p)
 pm.append(m)
 pm.append(d)
 pm.append(p2)
 pm.append(m2)
-pm.append(d2)
-print(pm)
+pm.append(d2)"""
