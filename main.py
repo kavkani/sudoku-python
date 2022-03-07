f7545d517745a9e8171d07cefff5673e4faca5a6
Bardia-Sohrabi <bardiasohrabi86@gmail.com>
2022-03-07 16:44:32 +0330

    3D with window setup

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ from ursina import *
 import generator
 import classes
 
-app = Ursina()
+app = Ursina(borderless=False)
 window.title = "Sudoku 3D"
 punch_sound = Audio('punch_sound', loop=False, autoplay=False)
 sudoku_parent = Entity(model=None, position=(-0.5, 0, 0))
 home_buttons = []
-window.fullscreen = True
+window.cog_button.enabled = False
 window.fps_counter.enabled = False
-window.exit_button.visible = False
+window.exit_button.enabled = False
 
 
 def home(scene_code=0):
-    window.title = "Sudoku 3D - Home"
     Sky(texture="bg")
     if scene_code == 1:
         for button in classes.sudoku_buttons:
@@ -24,14 +23,13 @@ def home(scene_code=0):
     account = Button(icon="account", scale=0.13, position=(0.7, 0.35), color=rgb(83, 221, 108))
     home_buttons.append(account)
     account.tooltip = Tooltip("Account")
-    new_game = Button(icon="s4", text="Start a New 3D Sudoku", text_origin=(0, -0.45), scale=(0.54, 0.63),
+    new_game = Button(icon="s4", text="Start a New 3D Sudoku", text_origin=(0, -0.45), scale=(0.49, 0.6),
                       color=rgb(64, 71, 109))
     home_buttons.append(new_game)
     new_game.on_click = Func(game)
 
 
 def game():
-    window.title = "Sudoku 3D - New Game"
     for button in home_buttons:
         destroy(button)
     sudoku_parent.rotation = (45, 0, -45)
@@ -49,7 +47,7 @@ def update():
         sudoku_parent.rotation_y += mouse.velocity[1] * 630
 
 
-exit_button = Button(color=color.red, text="Quit", position=(0.83, 0.46))
+exit_button = Button(color=color.red, text="Quit", position=(0.82, 0.46))
 exit_button.fit_to_text(0.15)
 exit_button.on_click = application.quit
 home()
